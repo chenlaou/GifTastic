3031a223b7171beb529b030bb05c7019bcfe1487
Chenla <chenla24@gmail.com>
2019-07-05 19:11:08 -0400

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,95 +1,89 @@
-// Code included inside $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute.
-$(document).ready(function(){
-    // inital array of gif examples
-    var gifs = ["afk", "jk", "bff", "ftw", "idk", "imo", "lol"];
-    console.log(gifs.length);
 
-    //display gif function re-renders the HTML to display the appropriate content
-    function displaygifview() {
-        var gifs = $(this).attr("data-name")
-        var apikey = JBkrVcPFLEYehopwCd9kAfZ6hfkbSVrH
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + apikey;
 
-        // We create an AJAX call for specific gif button being clicked
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function(response) {
-            $("#gifview").empty();
+        $(document).ready(function(){
 
-            // storing the data from the AJAX request in the results variable
-            var results = response.data;
-            console.log(response);
-
-            //Loop the gifs to 10 total limit
-            for(var i = 0; i < results.length; i++) {
-
-                // Div to hold gifs
-                var gifDiv = $("<div>");
-                // style.css class
-                gifDiv.addClass("gifs");
-                // Creates ratings to be displayed
-                var rating = results[i].rating;
-                var p = $("<h2>").text("Rating: " + rating);
-
-                // Gif images to remain still or animate
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_still.url);
-                gifImage.attr("data-still", results[i].images.fixed_height_still.url);
-                gifImage.attr("data-animate", results[i].images.fixed_height.url);
-                gifImage.attr("data-state", "still");
-                gifImage.addClass('gifImage');
+            var displayedButtons = ["IDK", "LOL", "WTF", "OMG"];
+        
+            function displayImg(){
+        
+                $("#display-images").empty();
+                var input = $(this).attr("data-name");
+                var limit = 10;
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=M755d535DRiRTLnwIqX4Q0X1gTjT5v71";   
+        
+                $.ajax({
+                    url: queryURL, 
+                    method: "GET"
+                }).done(function(response) {
+        
+                    for(var j = 0; j < limit; j++) {    
+        
+                        var displayDiv = $("<div>");
+                        displayDiv.addClass("holder");
+                    
+                        var image = $("<img>");
+                        image.attr("src", response.data[j].images.original_still.url);
+                        image.attr("data-still", response.data[j].images.original_still.url);
+                        image.attr("data-animate", response.data[j].images.original.url);
+                        image.attr("data-state", "still");
+                        image.attr("class", "gif");
+                        displayDiv.append(image);
+        
+                        var rating = response.data[j].rating;
+                        console.log(response);
+                        var pRating = $("<p>").text("Rating: " + rating);
+                        displayDiv.append(pRating)
+        
+                        $("#display-images").append(displayDiv);
+                    }
+                });
             }
-            //display the rating
-            gifDiv.prepend(p);
-            //display the gif image
-            gifDiv.prepend(gifImage);
-            $("#gifview")
-            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
-            
-            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-            // Then, set the image's data-state to animate
-            // Else set src to the data-still value
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }
-            // function for displaying gif data
-            function renderButtons() {
-                $("#gifbuttons").empty();
-                for(var i = 0; i < gifs.length; i++) {
-                    //generate buttons using jQuery
-                    var gifAdd = $("<button>");
-                    gifAdd.addClass("trending")
-                    //adding data-attr
-                    gifAdd.attr("data-name", gifs[i]);
-                    // button text   
-                    gifAdd.text(gifs[i]);
-                    // appending to buttons view div
-                    $("#gifbuttons").append(gifAdd)
+        
+            function renderButtons(){ 
+        
+                $("#display-buttons").empty();
+        
+                for (var i = 0; i < displayedButtons.length; i++){
+        
+                    var newButton = $("<button>") 
+                    newButton.attr("class", "btn btn-default");
+                    newButton.attr("id", "input")  
+                    newButton.attr("data-name", displayedButtons[i]); 
+                    newButton.text(displayedButtons[i]); 
+                    $("#display-buttons").append(newButton); 
                 }
             }
-            // Gif Button on-click function
-            $("add-gif").on("click"), function (event) {
-                event.preventDefault();
-                // Grabbing the input plus cutting out white space (.trim)
-                var gif = $("#gif-input").val().trim();
-                // adding a new input into the array
-                gifs.push(gif);
-
-                // Calling on the renderButtons to process our gifs on click
+        
+            function imageChangeState() {          
+        
+                var state = $(this).attr("data-state");
+                var animateImage = $(this).attr("data-animate");
+                var stillImage = $(this).attr("data-still");
+        
+                if(state == "still") {
+                    $(this).attr("src", animateImage);
+                    $(this).attr("data-state", "animate");
+                }
+        
+                else if(state == "animate") {
+                    $(this).attr("src", stillImage);
+                    $(this).attr("data-state", "still");
+                }   
+            }
+        
+            $("#submitPress").on("click", function(){
+        
+                var input = $("#user-input").val().trim();
+                form.reset();
+                displayedButtons.push(input);
+                        
                 renderButtons();
-            };
-            // event click lisenter for all elements for .gif
-            $(document).on("click", ".gif", displaygifview);
-
-            // display for inital buttons
+        
+                return false;
+            })
+        
             renderButtons();
-
+        
+            $(document).on("click", "#input", displayImg);
+            $(document).on("click", ".gif", imageChangeState);
         });
-    };
-})
